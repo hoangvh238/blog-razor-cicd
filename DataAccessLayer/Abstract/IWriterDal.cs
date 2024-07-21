defd2f647947dcdfd8d74193a8979c9f72896189
Letu123456 <144123103+Letu123456@users.noreply.github.com>
2024-07-21 23:14:02 +0700

    Delete DataAccessLayer directory

--- DataAccessLayer/Abstract/IWriterDal.cs
+++
@@ -1,9 +0,0 @@
-﻿using EntityLayer.Concrete;
-
-namespace DataAccessLayer.Abstract
-{
-    public interface IWriterDal : IGenericDal<Writer>
-    {
-        Writer GetWriterBySession(string session);
-    }
-}
