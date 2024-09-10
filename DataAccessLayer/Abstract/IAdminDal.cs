905c41425183fe2cddd42d09220232e4490390df
Vo Huy Hoang <hoangvh238.dev@gmail.com>
2024-09-10 18:37:00 +0700

    Update IAdminDal.cs

--- DataAccessLayer/Abstract/IAdminDal.cs
+++ DataAccessLayer/Abstract/IAdminDal.cs
@@ -5,6 +5,7 @@ namespace DataAccessLayer.Abstract
     public interface IAdminDal
     {
         List<int> SmallQueryData(int id);
+        
         List<int> ComplexQueryData();
         List<string> GetRoles();
     }
